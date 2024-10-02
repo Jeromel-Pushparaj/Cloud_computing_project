a4908a1b410e1c512fcb31dbee8757c0da0b981d
Jeromel Pushparaj <116452699+Jeormel@users.noreply.github.com>
2024-10-02 15:54:29 +0530

    first commit

---
+++ cloud_web/app/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, render_template
+
+main = Blueprint('main', __name__)
+
+@main.route('/')
+def index():
+    return render_template('index.html')
+
+@main.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@main.route('/page2')
+def page2():
+    return render_template('page2.html')
+
+@main.route('/page3')
+def page3():
+    return render_template('page3.html')
+
+@main.route('/page4')
+def page4():
+    return render_template('page4.html')
